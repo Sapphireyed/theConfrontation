496b130b647474740826d9a0655d0a9d4f8bc926
Gosia Szulc <sapphireyed@gmail.com>
2024-11-12 10:01:53 +0100

    setup done,cards initiated, movements need fixing as chars cords don't update after shuffle

--- utils/calculations.py
+++ utils/calculations.py
@@ -2,7 +2,6 @@ def get_char_coordinates_init(win, reg_in, char, side):
     if reg_in.limit == 2:
         x = reg_in.x + reg_in.width / 2 - char.width / 2
         y = reg_in.y + 10 if reg_in.chars[0].name.lower() == char.name.lower() else reg_in.y + 50
-        char.draw(win, (x, y), side)
     elif reg_in.limit == 4:
         x, y = 0, 0
         if char.name.lower() == reg_in.chars[0].name.lower():
